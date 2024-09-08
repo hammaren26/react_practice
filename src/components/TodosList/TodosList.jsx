c1869b6ed4b4fcd08df2e77cc22e783d6b0415ac
Vyacheslav <hammaren23@gmail.com>
2024-09-08 16:46:48 +0600

    initial commit

--- src/components/TodosList/TodosList.jsx
+++ src/components/TodosList/TodosList.jsx
@@ -10,6 +10,26 @@ export default function TodosList({ todosList, setTodosList }) {
     return Number(a.done) - Number(b.done);
   });
 
+  function toCamelCase(str) {
+    let arr = str.split(/[-_]/);
+
+    arr.forEach((element, index) => {
+      if (index > 0) {
+        arr[index] = element.charAt(0).toUpperCase() + element.slice(1);
+      }
+    });
+
+    return arr.join("");
+  }
+
+  let str = "the-stealth-warrior";
+  let str2 = "The_Stealth_Warrior";
+  let str3 = "The_Stealth-Warrior";
+
+  console.log(toCamelCase(str));
+  console.log(toCamelCase(str2));
+  console.log(toCamelCase(str3));
+
   return (
     <>
       {todosList.length ? (
