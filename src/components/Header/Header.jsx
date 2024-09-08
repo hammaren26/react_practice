128d626fc34fe489da65f0fe936ddd11a5d9410b
Vyacheslav <hammaren23@gmail.com>
2024-09-08 17:12:52 +0600

    sdfsdfsdf

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,7 @@
 import styles from "./header.module.css";
 
 export default function Header() {
-  return <header className={styles.header}>my todo list</header>;
+  return (
+    <header className={styles.header}>my todo list and test string</header>
+  );
 }
